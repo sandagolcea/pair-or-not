5bcd357db1c7634a5abbc95d0faec57caef9a7bf
Sanda Golcea <sanda.golcea@gmail.com>
2015-01-20 23:14:12 +0000

    Adds README

---
+++ README.md
@@ -0,0 +1,6 @@
+##Pair Or Not
+A project to help you choose who you want to pair with and get in touch with them.
+
+####Technologies used so far
+* Ruby
+* Sinatra
--- lib/like.rb
+++ lib/like.rb
@@ -4,21 +4,3 @@ class Like
   belongs_to :source, 'User', :key => true
   belongs_to :targer, 'User', :key => true
 end
-
-
-# 1 class Person
-#  2   include DataMapper::Resource
-#  3 
-#  4   property :id,    Serial
-#  5   property :name , String, :required => true
-#  6 
-#  7   has n, :friendships, :child_key => [ :source_id ]
-#  8   has n, :friends, self, :through => :friendships, :via => :target
-#  9 end
-# 10 
-# 11 class Friendship
-# 12   include DataMapper::Resource
-# 13 
-# 14   belongs_to :source, 'Person', :key => true
-# 15   belongs_to :target, 'Person', :key => true
-# 16 end
