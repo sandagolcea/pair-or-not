ac75d400b1e8c50380b76b0ee311479df1af502c
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-03 21:52:00 +0000

    Data mapping for friendship relationships changed

--- lib/like.rb
+++ lib/like.rb
@@ -2,5 +2,5 @@ class Like
   include DataMapper::Resource
 
   belongs_to :source, 'User', :key => true
-  belongs_to :targer, 'User', :key => true
+  belongs_to :target, 'User', :key => true
 end
--- lib/user.rb
+++ lib/user.rb
@@ -10,17 +10,17 @@ class User
   property :twitter_handle, String
 
   has n, :likes, :child_key => [ :source_id ]
+  has n, :likes_me, :child_key => [ :target_id ]
   has n, :matches, self, :through => :likes, :via => :target
 
   has n, :messages_sent_by_me, 'Message', :child_key => [:sender_id]
   has n, :messages_received_by_me, 'Message', :child_key => [:recipient_id]
 
-  has n, :sent_messages, self,
-   :through => :messages_sent_by_me, 
-   :via     => :sender
+  # has n, :sent_messages, self,
+  #  :through => :messages_sent_by_me, 
+  #  :via     => :sender
 
-  has n, :received_messages, self,
-   :through => :messages_received_by_me,
-   :via     => :recipient
-
+  # has n, :received_messages, self,
+  #  :through => :messages_received_by_me,
+  #  :via     => :recipient
 end
